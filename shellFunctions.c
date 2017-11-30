aa4462ad46afb1fda5dac04d3d049a60173fb67e
Jessica Kanczura <jkanczura@albany.edu>
2017-11-29 20:17:42 -0500

    Create shellFunctions.c

---
+++ shellFunctions.c
@@ -0,0 +1,7 @@
+/*
+Daniel Hug dhug@albany.edu: Recoder
+Alana Ruth Aruth@albany.edu : Monitor
+Jessica Kanczura jKanczura@albany.edu : Leader
+
+Contains functions for handling commands for the shell only
+*/
