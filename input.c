809ca22387d36289651918e3d7fc9b8f674b9706
Jessica Kanczura <jkanczura@albany.edu>
2017-11-29 20:21:59 -0500

    Create input.c

---
+++ input.c
@@ -0,0 +1,7 @@
+/*
+Daniel Hug dhug@albany.edu: Recoder
+Alana Ruth Aruth@albany.edu : Monitor
+Jessica Kanczura jKanczura@albany.edu : Leader
+
+reads line from specified file and returns result as a null terminated string
+*/
