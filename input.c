1941e2d0df2937337e4501f9b091be91cd0e48a1
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-12-05 22:22:02 -0500

    Hopefully works

--- input.c
+++ input.c
@@ -3,5 +3,26 @@ Daniel Hug dhug@albany.edu: Recoder
 Alana Ruth Aruth@albany.edu : Monitor
 Jessica Kanczura jKanczura@albany.edu : Leader
 
-reads line from specified file and returns result as a null terminated string
+Reads line from specified file and returns result as a null terminated string
 */
+#include <stdio.h>
+
+char * getLine(FILE *stream);
+
+//Function reads one line from the FILE and returns it as a null terminated string
+char * getLine(FILE *stream) {
+
+	//Line pointer set to the first line of the file
+	char *line = fgets(line, sizeof(char) * 1024, stream);
+
+	//If an error occurred, print message and return NULL
+	if (line == NULL){
+		fprintf(stderr, "Error: Line could not be read from file stream.\n");
+		return NULL;
+	}
+
+	//Otherwise, return the line followed by a null termination
+	*line++ = '\0';
+	return line;
+
+}
