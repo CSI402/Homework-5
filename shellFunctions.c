63a1242e60937a1aba9b3aff8070c510a39df71c
Dan Hug <dhug@albany.edu>
2017-12-07 17:39:27 -0500

    Add files via upload
    adding shell functions

--- shellFunctions.c
+++ shellFunctions.c
@@ -6,12 +6,17 @@ Jessica Kanczura jKanczura@albany.edu : Leader
 Contains functions for handling commands for the shell only
 */
 
-#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h> /* for fork */
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
 
 //Function prototypes
 void wd();
 void chwd(char *pathName);
 void quit();
+void execute(char ** argList);
+void parse (char *line, char **argv);
 
 //Prints the absolute path name of the current working directory
 void wd() {
@@ -36,5 +41,57 @@ void chwd(char *pathName) {
 
 //Prints "goodbye", and simple shell should stop processing commands (break out of loop and exit)
 void quit() {
-	printf("goodbye\n");
+	printf("Goodbye\n");
+
 }
+
+void parse(char *line, char **argv)
+{
+     while (*line != '\0') {       /* if not the end of line ....... */ 
+          while (*line == ' ' || *line == '\t' || *line == '\n')
+               *line++ = '\0';     /* replace white spaces with 0    */
+          *argv++ = line;          /* save the argument position     */
+          while (*line != '\0' && *line != ' ' && 
+                 *line != '\t' && *line != '\n') 
+               line++;             /* skip the argument until ...    */
+     }
+     *argv = '\0';                 /* mark the end of argument list  */
+}
+
+
+void execute(char ** argv){
+
+	pid_t pid;
+	pid_t c;
+	int cstatus;
+
+	pid = fork();
+
+	if (pid < 0) {
+		fprintf(stderr, "ERROR: fork failed.\nExiting...\n");
+		exit(-1);
+	}
+	if (pid == 0) {
+		execvp(*argv, argv);
+		fprintf(stderr, "Child process could not execute execvp.\n");
+		exit(-1);
+	}
+
+	else {
+		c = wait(&cstatus);
+		char * status;
+
+	if(cstatus == 0){
+		status = strdup("success!");
+	}	
+	else{
+		status = strdup("ERROR (!= 0).");
+	}
+
+	if(cstatus != 0){
+		printf("Exit status: %d implies %s\n\n", cstatus, status); 
+	}
+
+}
+}
+
--- simpleShell.c
+++ simpleShell.c
@@ -5,3 +5,101 @@ Jessica Kanczura jKanczura@albany.edu : Leader
 
 Main function for shell program
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> /* for fork */
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
+#include "shellFunctions.c"
+//#include "input.c"
+
+int main(int argc, char * argv[]) {
+	char command[1024] ;
+	char *fileName = malloc(sizeof(char*));
+	//Declare a FILE pointer variable
+ 	FILE *fp;
+	if(argc == 1){
+		while(1){
+			printf("$> ");
+			gets(command);
+			parse(command, argv);
+			//save command location to argv
+			//if strcmp(argv[0], "list")
+			if(strcmp(argv[0], "list") == 0){
+				printf("This is the list function\n");
+				execute(argv);
+			}
+			else if(strcmp(command, "create") == 0){
+				printf("This is the create function\n");
+				//fork function
+			}
+			else if(strcmp(command, "wd") == 0){
+				printf("This is the wd function\n");
+				wd();
+			}
+			else if(strcmp(argv[0], "chwd") == 0){
+				printf("This is the chwd function %s \n", argv[1]);
+
+				chwd(argv[1]);
+			}
+			else if(strcmp(command, "quit") == 0){
+				quit();
+				return 0;
+				
+			}
+			else{
+				fprintf(stderr, "Error: Invalid Command (%s).\n", command);
+			}									
+		}
+	}
+
+/*
+	if(argc == 2){
+		fileName = argv[2];
+   		//If given file cannot be opened, print error message and stop
+   		if((fp = fopen(fileName, "r+"))== NULL){
+     	fprintf(stderr, "Error: File %s cannot be opened.\n", fileName);
+    	return 0;
+  		}
+  		while(fp != EOF){
+			printf("$> ");
+			command = getLine(fp);
+
+			if(strcmp(command, "list") == 0){
+				printf("This is the list function\n");
+				system("./list");
+			}
+			else if(strcmp(command, "create") == 0){
+				printf("This is the create function\n");
+				//fork function
+			}
+			else if(strcmp(command, "wd") == 0){
+				printf("This is the wd function\n");
+				wd();
+			}
+			else if(strcmp(command, "chwd") == 0){
+				printf("This is the chwd function\n");
+				chwd("/users/danielhug");
+			}
+			else if(strcmp(command, "quit") == 0){
+				quit();
+			}
+			else{
+				fprintf(stderr, "Error: Invalid Command (%s).\n", command);
+			}									
+		}
+	}
+
+*/
+}
+
+
+
+
+
+
+
+
+
+
